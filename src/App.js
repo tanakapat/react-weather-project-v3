3667109d8dbd6a51c83387e4307621cc1fc74eb2
tanakapat <patricia.tanaka.3@hotmail.com>
2025-03-09 14:38:08 -0300

    Added the Github url

--- src/App.js
+++ src/App.js
@@ -7,6 +7,9 @@ export default function App() {
       <h1>Weather App</h1>
 
       <h2>New weather app</h2>
+      <footer>Coded by Patricia Tanaka and it is open sourced on <a href="https://github.com/tanakapat/react-weather-project-v3" rel="noreferrer" target="_blank" >Github</a>
+
+      </footer>
     </div>
   );
 }
